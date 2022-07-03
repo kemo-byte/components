cea6f7a95ea82afbbe772cf7dfea83a5a1a3abf1
kemobyte <kamalkafi12@gmail.com>
2022-07-03 06:10:47 +0200

    add bootstrap 5 and section component

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,8 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title','default')</title>
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/all.min.css')}}" />
 </head>
 <body>
     {{ $slot}}
+
+    <script src="{{ asset('js/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{ asset('js/all.min.js')}}"></script>
 </body>
 </html>
--- resources/views/flash.blade.php
+++ resources/views/flash.blade.php
@@ -1,3 +1,20 @@
 <x-layout>
-    Hello World
+    <section class="bg-danger bg-gray p-3 text-center text-white mb-2">
+        <div class="container">
+            Hello World
+        </div>
+    </section>
+    
+    <section class="bg-danger bg-gray p-3 text-center text-white mb-2">
+        <div class="container">
+            Hello World
+        </div>
+    </section>
+    
+    <section class="bg-danger bg-gray p-3 text-center text-white mb-2">
+        <div class="container">
+            Hello World
+        </div>
+    </section>
+    
 </x-layout>
